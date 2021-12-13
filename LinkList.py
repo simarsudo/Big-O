330c5164f78f5a054c95c8792fa0d0aefbb709a5
Simarjeet <sjthecoder@gmail.com>
2021-12-13 16:22:34 +0530

    initial LinkList.py create and basic classes add

---
+++ LinkList.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self, value):
+        if self.head is None:
+            print('Linked list is already empty')
+            return False
+        elif self.tail.next is None:
+
+
+
+
+my_kinked_list = LinkedList(4)
+
+my_kinked_list.append(5)
+
+print(my_kinked_list.print_list())
