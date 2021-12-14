c479f4980c01f96ed180027550057e5ddd9a5e75
Simarjeet <sjthecoder@gmail.com>
2021-12-14 16:24:31 +0530

    add pop and prepend methods

--- DoubleLinkList.py
+++ DoubleLinkList.py
@@ -33,9 +33,41 @@ class DoubleLinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        if self.head is None:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.tail = None
+            self.head = None
+        else:
+            self.tail = temp.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+
+
+# Test
 my_double_link_list = DoubleLinkedList(7)
 my_double_link_list.append(1)
 my_double_link_list.append(15)
 my_double_link_list.append(19)
+
+print(my_double_link_list.print_list())
+
+my_double_link_list.prepend(69)
+my_double_link_list.prepend(420)
 print(my_double_link_list.print_list())
