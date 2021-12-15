ba9af36ee7c6185f1bc84f5e28237264110f815b
Simarjeet <sjthecoder@gmail.com>
2021-12-15 23:58:44 +0530

    add contains method

--- Tree.py
+++ Tree.py
@@ -29,12 +29,25 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
+    def contains(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            return False
+        temp = self.root
+        while temp is not None:
+            if new_node.value < temp.value:
+                temp = temp.left
+            elif new_node.value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
 
 my_tree = BinarySearchTree()
 my_tree.insert(2)
 my_tree.insert(1)
 my_tree.insert(3)
 
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
+
+print(my_tree.contains(69))
