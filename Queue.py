341ccb0332b4ba6d9a1678040a4404949ac2b5de
Simarjeet <sjthecoder@gmail.com>
2021-12-15 18:13:53 +0530

    add enqueue method

--- Queue.py
+++ Queue.py
@@ -12,8 +12,22 @@ class Queue:
         self.length = 1
 
     def print_queue(self):
-        temp = self.last
+        temp = self.first
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
+
+my_queue = Queue(1)
+my_queue.enqueue(2)
+print(my_queue.print_queue())
