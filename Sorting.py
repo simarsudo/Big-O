554ce57d33fe0acf372816995d8ab7f0c45cca2c
Simarjeet <sjthecoder@gmail.com>
2021-12-16 23:13:44 +0530

    selection sort added

--- Sorting.py
+++ Sorting.py
@@ -6,6 +6,17 @@ def bubble_sort(my_list):
     return my_list
 
 
+def selection_sort(my_list):
+    for i in range(len(my_list) - 1):
+        min_index = i
+        for j in range(i + 1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if i != min_index:
+            my_list[i] , my_list[min_index] = my_list[min_index], my_list[i]
+    return my_list
+
+
 w_list = [4, 2, 6, 5, 1, 3]
 
-print(bubble_sort(w_list))
+print(selection_sort(w_list))
