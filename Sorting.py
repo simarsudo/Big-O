82a39620197817f515f02779343f2c263b4e6307
Simarjeet <sjthecoder@gmail.com>
2021-12-16 23:47:16 +0530

    insertion sort added

--- Sorting.py
+++ Sorting.py
@@ -17,6 +17,17 @@ def selection_sort(my_list):
     return my_list
 
 
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i-1
+        while temp < my_list[j] and j > -1:
+            my_list[j + 1] = my_list[j]
+            my_list[j] = temp
+            j -=1
+    return my_list
+
+
 w_list = [4, 2, 6, 5, 1, 3]
 
-print(selection_sort(w_list))
+print(insertion_sort(w_list))
