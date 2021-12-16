188ed1769d141b9a3c259a2e6de9a954b68077a5
Simarjeet <sjthecoder@gmail.com>
2021-12-16 19:54:17 +0530

    add remove_edge method

--- Graphs.py
+++ Graphs.py
@@ -19,11 +19,28 @@ class Graphs:
             return True
         return False
 
+    def remove_edge(self, v1, v2):
+        try:
+            if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+                return True
+        except ValueError:
+            pass
+        return False
+
 
 mygraph = Graphs()
 
-mygraph.add_vertex(1)
-mygraph.add_vertex(2)
-mygraph.add_edge(1, 23)
+mygraph.add_vertex('A')
+mygraph.add_vertex('B')
+mygraph.add_vertex('C')
+mygraph.add_vertex('D')
+
+mygraph.add_edge('A', 'B')
+mygraph.add_edge('B', 'C')
+mygraph.add_edge('C', 'A')
+
+mygraph.remove_edge('D', 'A')
 
 mygraph.print_graphs()
