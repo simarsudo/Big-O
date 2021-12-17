36c790ddfc6ee091afcd18079f6500804db6748c
Simarjeet <sjthecoder@gmail.com>
2021-12-17 19:05:07 +0530

    bfs_in_order method add

--- Tree.py
+++ Tree.py
@@ -61,7 +61,7 @@ class BinarySearchTree:
                 queue.append(current_node.right)
         return results
 
-    def bsf_pre_order(self):
+    def bfs_pre_order(self):
         results = []
 
         def traverse(current_node):
@@ -74,7 +74,7 @@ class BinarySearchTree:
         traverse(self.root)
         return results
 
-    def bsf_post_order(self):
+    def bfs_post_order(self):
         results = []
 
         def traverse(current_node):
@@ -87,14 +87,15 @@ class BinarySearchTree:
         traverse(self.root)
         return results
 
+    def bfs_in_order(self):
+        results = []
 
-my_tree = BinarySearchTree()
-my_tree.insert(52)
-my_tree.insert(12)
-my_tree.insert(32)
-my_tree.insert(43)
-my_tree.insert(4)
-my_tree.insert(65)
-my_tree.insert(82)
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
 
-print(my_tree.bsf_post_order() )
+        traverse(self.root)
+        return results
