3e8609fd6fb294bdfe338e5cf7cb9e737e4ca68b
Simarjeet <sjthecoder@gmail.com>
2021-12-17 15:23:14 +0530

    merge add

--- Sorting.py
+++ Sorting.py
@@ -13,18 +13,37 @@ def selection_sort(my_list):
             if my_list[j] < my_list[min_index]:
                 min_index = j
         if i != min_index:
-            my_list[i] , my_list[min_index] = my_list[min_index], my_list[i]
+            my_list[i], my_list[min_index] = my_list[min_index], my_list[i]
     return my_list
 
 
 def insertion_sort(my_list):
     for i in range(1, len(my_list)):
         temp = my_list[i]
-        j = i-1
+        j = i - 1
         while temp < my_list[j] and j > -1:
             my_list[j + 1] = my_list[j]
             my_list[j] = temp
-            j -=1
+            j -= 1
     return my_list
 
+
+def merge(list1, list2):
+    combined = []
+    i = 0
+    j = 0
+    while i < len(list1) and j < (len(list2)):
+        if list1[i] < list2[j]:
+            combined.append(list1[i])
+            i += 1
+        else:
+            combined.append(list2[j])
+            j += 1
+    while i < len(list1):
+        combined.append(list1[i])
+        i += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 1
+    return combined
 
