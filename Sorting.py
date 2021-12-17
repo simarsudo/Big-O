678c63c59c7f6e075bb22ad3242d17d639927160
Simarjeet <sjthecoder@gmail.com>
2021-12-17 17:47:35 +0530

    add quick sort

--- Sorting.py
+++ Sorting.py
@@ -51,7 +51,37 @@ def merge(list1, list2):
 def merge_sort(my_list):
     if len(my_list) == 1:
         return my_list
-    mid = int(len(my_list)/2)
+    mid = int(len(my_list) / 2)
     left = my_list[:mid]
     right = my_list[mid:]
     return merge(merge_sort(left), merge_sort(right))
+
+
+def swap(my_list, index1, index2):
+    temp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = temp
+
+
+def pivot(my_list, pivot_index, end_index):
+    swap_index = pivot_index
+
+    for i in range(pivot_index+1, end_index+1):
+        if my_list[i] < my_list[pivot_index]:
+            swap_index += 1
+            swap(my_list, swap_index, i)
+    swap(my_list, pivot_index, swap_index)
+    return swap_index
+
+
+def quick_sort_helper(my_list, left, right):
+    if left < right:
+        pivot_index = pivot(my_list, left, right)
+        quick_sort_helper(my_list, left, pivot_index - 1)
+        quick_sort_helper(my_list, pivot_index+1, right)
+    return my_list
+
+
+def quick_sort(my_list):
+    return quick_sort_helper(my_list, 0, len(my_list) -1)
+
